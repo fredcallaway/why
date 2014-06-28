3d61fbba8999acf31d5c8685a333a25f9fa1ff0b
erin <festethejester@gmail.com>
2014-06-27 17:11:21 -0700

    add back present functionality

--- experiments/_shared/js/stream-V2.js
+++ experiments/_shared/js/stream-V2.js
@@ -1,91 +1,85 @@
 /// Stream ///
 
 var slide = function (_slide, _private) {
-    var s = _slide || {}, p = _private || {};
-    /*//add method to all stimuli bearing slides
-    s.phaseid=0;
-    s.data = [];*/
-    s.init = function() {
-        utils.showSlide(this.name);
-        /*$('#progress').css('visibility', 'hidden');*/
-        //this.phaseid++;
-        if (this.start) this.start();
-        /*if (s.present != undefined)  {
-            s.takeInput = 1;
-            s.resp = [];
-            if (s.slen == undefined) s.slen = s.present.length;
-        }
+  var s = _slide || {}, p = _private || {};
+  //add method to all stimuli bearing slides
+  s.phaseid=0;
+  s.init = function() {
+    utils.showSlide(this.name);
+    /*$('#progress').css('visibility', 'hidden');*/
+    this.phaseid++;
+    if (this.start) {this.start();};
+    /*if (s.present !== undefined)  {
+        if (s.slen == undefined) s.slen = s.present.length;
+    };*/
+    //exp.phase++;
 
-        if(this.prog) {
+    /*if(this.prog) {
+        $('#progress').css('visibility', 'visible');
+        if (this.progress){
+            this.progress();
+        }
+        else if (exp.progress){
+            exp.progress();
+        }
+        else{
+            exp.phase++;
+            //$('#phase').text("Question    " + exp.phase + " of " + exp.prog);
+            $('#phase').text( exp.phase + " of " + exp.prog);
             $('#progress').css('visibility', 'visible');
-            if (this.progress){
-                this.progress();
-            }
-            else if (exp.progress){
-                exp.progress();
-            }
-            else{
-                exp.phase++;
-                //$('#phase').text("Question    " + exp.phase + " of " + exp.prog);
-                $('#phase').text( exp.phase + " of " + exp.prog);
-                $('#progress').css('visibility', 'visible');
-            }
         }
-        if(this.handle) this.handle();
+    }*/
+    if(this.handle) this.handle();
+    _stream.apply(this);
+  };
 
-        _stream.apply(this);*/
-    };
-    /*s.pass_data = function(handle){
-        return function(_extensions) {
-            if(this.data_handle) var x= this.data_handle();
-            else x = {}
-            //            var x = this.data_handle() || {},
-            var e = _extensions || {},
-                time = getTime(),
-                data = {cond: exp.condition, phaseid: this.phaseid, phase: this.name, t:time, t_start:new Date(), catch: 0};
-            _.extend(data, x, e);
-            exp.data[exp.data.length]= data;
-            this.data[this.data.length]=data;
-        };
-    }(s.data_handle);*/
-
-    s.callback = s.callback !== undefined ? s.callback : function() {exp.go();};
-    return(s);
+  //what to do when done presenting all the slides
+  s.callback = s.callback !== undefined ? s.callback : function() {exp.go();};
+  return(s);
 };
 
 var _stream = function() {
-    /*if (this.present == undefined) { //not a presented slide
-        this.pass_data();
-    }
+  if (this.present == undefined) { //not a presented slide (i.e. there are not multiple trials using the same slide)
+    //this.pass_data();
+  } else {
+    var presented_stims = this.present || [];
 
-    else { //a presented stim*/
-        var presented_stims = this.present || [];
+    //done with slide
+    if (presented_stims.length === 0) {
+        if (this.end) {this.end();};
+        this.callback();
+    } else {
+      if (this.present_handle) {
+        var stim = presented_stims.shift();
+        //var trial_num = this.slen - presented_stims.length;
+        this.present_handle(stim);
+        exp.phase++;
 
-        //done with slide
-        if (presented_stims.length === 0) {
-            if (this.end) this.end();
-            this.callback();
+        /*//Catch Trial
+        if (this.catch_trial_handle && stim.length == 3) {
+          //s = Math.round(Math.random());
+          //this.pass_data({trial:tri, stim:s, catch: 1});
+          this.catch_trial_handle(presented_stims[Math.floor(Math.random()*presented_stims.length)]);
         }
+
+        //Normal Trial
+        else{
+          //this.pass_data({"trial":trial_num, "stim":stim});
+          this.present_handle(stim);/*, function() {
+            this.takeInput = 1;
+          });
+        }*/
+      }
+      /*if (this.update_progress) {
+        this.update_progress();
+      }*/
+    }
         /*
         //stims left
         else {
             if (this.present_handle)  {
                 var s = presented_stims.shift();
                 var tri = this.slen-presented_stims.length;
-
-                //Catch Trial
-                if (this.catch_trial_handle && s.length == 3) {
-                    s = Math.round(Math.random());
-                    this.pass_data({trial:tri, stim:s, catch: 1});
-                    this.catch_trial_handle(presented_stims[Math.floor(Math.random()*presented_stims.length)]);
-                }
-                //Normal Trial
-                else{
-                    this.pass_data({trial:tri, stim:s});
-                    this.present_handle(s, function() {
-                        this.takeInput = 1;
-                    });
-                }
 
                 if (this.prog) {
                     if (this.update_progress){
@@ -114,4 +108,5 @@ var _stream = function() {
     //add trial time
     if (exp.data[exp.data.length-3] != undefined && exp.data[exp.data.length-3].t_start) exp.data[exp.data.length-2].trial_time =  getTimeOffset(exp.data[exp.data.length-3].t_start);
     //if (exp.data.length >= 2) exp.data[exp.data.length-2].trial_time =  getTimeOffset(exp.data[exp.data.length-3].t_start);*/
+  }
 };
