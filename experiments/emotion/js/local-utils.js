b1f9db06782b3df2cefaeec1126f1e76f3bcc05c
feste <festethejester@gmail.com>
2014-07-03 13:55:07 -0700

    fix slide height

---
+++ experiments/emotion/js/local-utils.js
@@ -0,0 +1,9 @@
+var exp_sizing = function(){
+    exp.width=800;
+    exp.height=620;
+    $('.slide').css('width', exp.width);
+    $('.slide').css('height', exp.height);
+    $('.exp').css('height', exp.height+2);
+    $('.slide_wrapper').css('width', exp.width);
+    $('.slide_wrapper').css('height', exp.height);
+}
